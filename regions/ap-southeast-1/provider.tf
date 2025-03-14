d69c16286469e4785ba5b7e628513fbca8a4b7da
maoshuai.17 <maoshuai.17@bytedance.com>
2025-03-14 11:57:12 +0800

    feat: create resource

---
+++ regions/ap-southeast-1/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    byteplus = {
+      source  = "byteplus-sdk/byteplus"
+      version = "=0.0.12"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "byteplus" {
+  region = var.region
+}
